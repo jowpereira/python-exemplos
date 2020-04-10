097f7f98d67e127f36f19044bf74cd9963d0b324
Joathan pereira <dahap@dot-coin.com>
2020-04-09 22:27:27 -0300

    ajustes

--- diversos/scraping.py
+++ diversos/scraping.py
@@ -8,22 +8,19 @@ import datetime as dt
 class Validation(object):
     """
     Exemplo de uso:
-    '''
-    https://www.forexfactory.com/calendar?day=today
-    https://www.metalsmine.com/calendar?day=today
 
-    '''
-    y = Validation("https://www.energyexch.com/calendar?day=today")
-    dataIni = dt.time(hour=8, minute=10)
-    dataFim = dt.time(hour=10, minute=35)
+    y = Validation()
+    dataIni = dt.time(hour=4, minute=10)
+    dataFim = dt.time(hour=23, minute=35)
 
-    if (y.validation(dataIni, dataFim, 'Low')):
+    if (y.validation(dataIni, dataFim, 'Low', 'CHF')):
         print('não negocia, está dentro do horario de noticias e impacto X')
     else:
         print('negocia, está fora do horario de noticias')
-    
+
     """
-    def __init__(self, url="https://www.metalsmine.com/calendar?day=today"):
+
+    def __init__(self, url="https://www.energyexch.com/calendar?day=today"):
         self.__url = url
 
     def __process_time(self, intime, start, end):
@@ -38,12 +35,12 @@ class Validation(object):
                 return True
         return False
 
-    def validation(self, initDate, endDate, impact):
+    def validation(self, initDate, endDate, impact, currency):
         for index, row in self.__prepareDate().iterrows():
             x = row.Time_Eastern
-            if x == 'Tentative' or x == 'All Day':
+            if x == 'Tentative' or x == 'All Day' or str(x).find('Day') is not -1:
                 continue
-            if self.__process_time(dt.time(hour=x.hour, minute=x.minute), dt.time(hour=initDate.hour, minute=initDate.minute), dt.time(hour=endDate.hour, minute=endDate.minute)) and self.__noticias[['Impact']].values[index] == impact:
+            if self.__process_time(dt.time(hour=x.hour, minute=x.minute), dt.time(hour=initDate.hour, minute=initDate.minute), dt.time(hour=endDate.hour, minute=endDate.minute)) and self.__noticias[['Impact']].values[index] == impact and str(self.__noticias['Currency']).find(currency) is not -1:
                 return True
 
         return False
@@ -51,7 +48,7 @@ class Validation(object):
     def __prepareDate(self):
         self.__noticias = self.__scraper()
         horasNoticia = pd.DataFrame(self.__noticias[["Time_Eastern"]])
-
+        
         for index, row in horasNoticia.iterrows():
             if row.Time_Eastern != '':
                 ultimaHonra = row
@@ -62,7 +59,7 @@ class Validation(object):
 
             if isinstance(m2, str):
                 m2 = m2.replace(u'\xa0', '')
-                if m2 == 'Tentative' or m2 == 'All Day':
+                if m2 == 'Tentative' or m2 == 'All Day' or m2.find('Day') is not -1:
                     continue
                 m2 = m2.replace("p", " p")
                 m2 = m2.replace("a", " a")
@@ -74,6 +71,7 @@ class Validation(object):
     def __scraper(self):
         session = requests.Session()
         session.max_redirects = 60
+        session.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
         page = session.get(self.__url)
         content = page.content
         soup = BeautifulSoup(content, "html.parser")
@@ -91,18 +89,23 @@ class Validation(object):
                         "span")[0]['title'].split(' ', 1)[0]
             except:
                 pass
-            dict["Actual"] = item.find_all("td", {"class": "calendar__actual"})[0].text      # Actual Value
-            dict["Forecast"] = item.find_all("td", {"class": "calendar__forecast"})[0].text  # forecasted Value
-            dict["Previous"] = item.find_all("td", {"class": "calendar__previous"})[0].text  # Previous
+            dict["Actual"] = item.find_all("td", {"class": "calendar__actual"})[
+                0].text      # Actual Value
+            dict["Forecast"] = item.find_all("td", {"class": "calendar__forecast"})[
+                0].text  # forecasted Value
+            dict["Previous"] = item.find_all("td", {"class": "calendar__previous"})[
+                0].text  # Previous
             forcal.append(dict)
         df = pandas.DataFrame(forcal)
         return df[["Currency", "Event", "Impact", "Time_Eastern", "Actual", "Forecast", "Previous"]]
-    
-y = Validation("https://www.energyexch.com/calendar?day=today")
-dataIni = dt.time(hour=8, minute=10)
-dataFim = dt.time(hour=10, minute=35)
 
-if (y.validation(dataIni, dataFim, 'Low')):
-    print('não negocia, está dentro do horario de noticias e impacto X')
-else:
-    print('negocia, está fora do horario de noticias')
+if __name__ == "__main__":
+
+    y = Validation("https://www.forexfactory.com/calendar?day=today")
+    dataIni = dt.time(hour=4, minute=10)
+    dataFim = dt.time(hour=23, minute=35)
+
+    if (y.validation(dataIni, dataFim, 'Low', 'AUD')):
+        print('não negocia, está dentro do horario de noticias e impacto X')
+    else:
+        print('negocia, está fora do horario de noticias')
